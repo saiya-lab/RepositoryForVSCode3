4faee780522e03c2ec28bcc2ff4b1c6b7d5e8ac6
saiya-lab <likedlem@gmail.com>
2025-09-06 16:12:37 +0900

    2025/09/06

--- atcodetest/Atcode2.py
+++ atcodetest/Atcode2.py
@@ -1,5 +1,17 @@
-a = int(input())
-if a % 2 == 0:
+#a = int(input("a_num:"))
+#a_num:3
+#print(a)
+#b = int(input("b_num:"))
+#b_num:4
+#if a * b % 2 == 0:
+    #print("Even")
+#else:
+    #print("Odd")
+
+a, b = map(int, input("値を入力してください:").split())
+print(f"あなたの入力:{a, b}")
+
+if a*b%2==0:
     print("Even")
 else:
     print("Odd")
