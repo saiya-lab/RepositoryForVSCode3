50c2678665f12c2eca04101087f0b2e8be382391
saiya-lab <likedlem@gmail.com>
2025-12-03 13:42:06 +0900

    2025/12/03/13

--- stickman/stickmangame.py
+++ stickman/stickmangame.py
@@ -13,7 +13,7 @@ class Coords:
     self.y1 = y1
     self.x2 = x2
     self.y2 = y2
-  
+#スプライトの衝突
   # def within_x(co1, co2):
   #   if co1.x1 >co2.x1 and co1.x1 < co2.x2:
   #     return True
@@ -26,16 +26,56 @@ class Coords:
   #   else:
   #     return False ↓と同義
 
-  def within_x(co1, co2):
-    if (co1.x1 > co2.x1 and co1.x1 < co2.x2) \
-      or (co1.x2 > co2.x1 and co1.x2 < co2.x2) \
-      or (co2.x1 > co1.x1 and co2.x1 < co1.x2) \
-      or (co2.x2 > co1.x1 and co2.x2 < co1.x2):
-      return True
-    else:
+#縦軸の衝突を判断するメソッド
+def within_x(co1, co2):
+  if (co1.x1 > co2.x1 and co1.x1 < co2.x2) \
+     or (co1.x2 > co2.x1 and co1.x2 < co2.x2) \
+     or (co2.x1 > co1.x1 and co2.x1 < co1.x2) \
+     or (co2.x2 > co1.x1 and co2.x2 < co1.x2):
+     return True
+  else:
       return False
 
+#横軸の衝突を判断するメソッド
+def within_y(co1, co2):
+  if(co1.y1 > co2.y1 and co1.y1 < co2.y2)\
+     or (co1.y2 > co2.y1 and co1.y2 < co2.y2)\
+     or (co2.y1 > co1.y1 and co2.y1 < co1.y2)\
+     or (co2.y2 > co1.y1 and co2.y2 < co1.y2):
+    return True
+  else:
+    return False
 
+#左側の衝突を調べるメソッド
+def collided_left(co1, co2):
+  if within_y(co1, co2):
+    if co1.x1 >= co2.x1 and co1.x1 <= co2.x2:
+      return True
+  return False
+
+#右側の衝突を調べるメソッド
+def collided_right(co1, co2):
+  if within_y(co1, co2):
+      if co1.y1 >= co2.y1 and co1.y1 <= co2.y2:
+        return True
+  return False
+
+#上側の衝突を調べるメソッド
+def collided_top(co1, co2):
+  if within_x(co1, co2):
+      if co1.y1 >= co2.y1 and co1.y1 <= co2.y2:
+        return True
+  return False
+
+#下側の衝突を調べるメソッド
+def collided_bottom(y, co1, co2):
+  if within_x(co1, co2):
+    y_calc =  co1, co2 + y
+    if y_calc >=co2.y1 and y_calc <= co2.y2:
+      return True
+  return False
+
+#ゲームループ
 class Game:
  def __init__(self):
     self.tk =Tk()
