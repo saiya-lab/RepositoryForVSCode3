9945a846a49e59e1ee9124687643b81bae62a80f
saiya-lab <likedlem@gmail.com>
2025-12-06 15:32:54 +0900

    {2025/12/06}

--- stickman/stickmangame.py
+++ stickman/stickmangame.py
@@ -75,6 +75,53 @@ def collided_bottom(y, co1, co2):
       return True
   return False
 
+#スプライトのクラス
+class Sprite:
+  def __init__(self, game):
+    self.game = game
+    self.endgame = False
+    self.coordinates = None
+
+  def move(self):
+    pass
+
+  def coords(self):
+    return self.coordinates
+
+#床クラス
+class PlatformSprite(Sprite):
+  def __init__(self, game, photo_image, x, y, width, height):
+    Sprite.__init__(self, game)
+    self.photo_image = photo_image
+    self.image = game.canvas.create_image(x, y,\
+                                          image=self.photo_image, anchor='nw')
+    self.coordinates = Coords(x, y, x + width, y + height)
+
+#スティックマン
+class StickFigureSprite(Sprite):
+  def __init__(self, game):
+    Sprite.__init__(self, game)
+    self.images_left = [
+      PhotoImage(file='figure-L1.gif'),
+      PhotoImage(file='figure-L2.gif'),
+      PhotoImage(file='figure-L3.gif')
+      ]
+    self.images_right = [
+      PhotoImage(file='figure-R1.gif'),
+      PhotoImage(file='figure-R2.gif'),
+      PhotoImage(file='figure-R3.gif')
+      ]
+    self.image = game.canvas.create_image(200, 470,
+            image=self.images_left[0], anchor='nw')
+    self.x = -2
+    self.y = 0
+    self.current_image = 0
+    self.current_image_add = 1
+    self.jump_count = 0
+    self.last_time = time.time()
+    self. coordinates = Coords()
+
+
 #ゲームループ
 class Game:
  def __init__(self):
@@ -85,11 +132,15 @@ class Game:
     self.canvas = Canvas(self.tk, width=500, height=500, highlightthickness=0)
     self.canvas.pack()
     self.tk.update()
+
     self.canvas_height = self.canvas.winfo_height()
     self.canvas_width = self.canvas.winfo_width()
+
     self.bg = PhotoImage(file='background.gif')
+
     w = self.bg.width()
     h = self.bg.height()
+
     for x in range(0, 5):
       for y in range(0, 5):
         self.canvas.create_image(x * w, y * h, image=self.bg, anchor='nw')
@@ -113,4 +164,18 @@ def mainloop(self):
     time.sleep(0.01)
 
 g = Game()
+
+platform1 = PlatformSprite(g, PhotoImage(file='platform1.gif'), 0, 480, 100, 10)
+platform2 = PlatformSprite(g, PhotoImage(file='platform1.gif'), 150, 440, 100, 10)
+platform3 = PlatformSprite(g, PhotoImage(file='platform1.gif'), 300, 400, 100, 10)
+platform4 = PlatformSprite(g, PhotoImage(file='platform1.gif'), 300, 160, 100, 10)
+platform5 = PlatformSprite(g, PhotoImage(file='platform1.gif'), 175, 350, 66, 10)
+platform6 = PlatformSprite(g, PhotoImage(file='platform1.gif'), 50, 300, 66, 10)
+platform7 = PlatformSprite(g, PhotoImage(file='platform1.gif'), 170, 120, 66, 10)
+platform8 = PlatformSprite(g, PhotoImage(file='platform1.gif'), 45, 60, 66, 10)
+platform9 = PlatformSprite(g, PhotoImage(file='platform1.gif'), 170, 250, 32, 10)
+platform10 = PlatformSprite(g, PhotoImage(file='platform1.gif'), 230, 200, 32, 10)
+
+g.sprites.append(platform1)
 g.tk.mainloop()
+#0,480,100,10は位置キャンバスの横0縦480ピクセルと画像の幅100、高さ10ピクセルを表している
