f9fe33d64ca8d2ef23adf299e6f054b788789d1e
saiya-lab <likedlem@gmail.com>
2025-12-24 12:46:15 +0900

    {2025/12/24}

--- java_practice/app.js
+++ java_practice/app.js
@@ -1,1 +1,18 @@
-alert("Hello World!");
+// テスト ;で文の終わり ctrl/コメント化
+//console.log("Hello World!");consoleはprintに近い動作確認
+// foo bar サンプルコードでよく使われる baz, qux, foobar, hoge, huga, piyo
+// コーテションはどちらでもいいが、一つのプロジェクトで片方しか使わない。
+//const foo = 1 + 1;
+//console.log(foo)
+console.log(1 + 1);
+//変数　const 変数名　=　値
+const foo = 1;
+console.log(foo)
+
+const colorDark = "#333"
+if(new Date().getHours() >= 0){
+    document.body.style.backgroundColor = colorDark;
+}
+if(user.setting.darkMode === true){
+    document.body.style.backgroudColor = colorDark;
+}
